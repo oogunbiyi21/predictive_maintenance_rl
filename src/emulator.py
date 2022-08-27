c9e69c1c1de4e68c5389c8bb7d3051e0c9b73505
togunbiyi1 <olatunjiogunbiyi@googlemail.com>
2022-08-27 10:13:19 +0100

    refactoring

--- src/emulator.py
+++ src/emulator.py
@@ -1,111 +1,140 @@
 from lib import *
+import pandas as pd
+import numpy as np
+import random, os
+
 
 # by Xiang Gao, 2018
 
 
-
-def find_ideal(p, just_once):
-	if not just_once:
-		diff = np.array(p[1:]) - np.array(p[:-1])
-		return sum(np.maximum(np.zeros(diff.shape), diff))
-	else:
-		best = 0.
-		i0_best = None
-		for i in range(len(p)-1):
-			best = max(best, max(p[i+1:]) - p[i])
-
-		return best
-
-
 class Market:
-	"""
-	state 			MA of prices, normalized using values at t
-					ndarray of shape (window_state, n_instruments * n_MA), i.e., 2D
-					which is self.state_shape
+    """
+    state 			MA of prices, normalized using values at t
+                    ndarray of shape (window_state, n_instruments * n_MA), i.e., 2D
+                    which is self.state_shape
 
-	action 			three action
-					0:	empty, don't open/close. 
-					1:	open a position
-					2: 	keep a position
-	"""
-	
-	def reset(self, rand_price=True):
-		self.empty = True
-		if rand_price:
-			prices, self.title = self.sampler.sample()
-			price = np.reshape(prices[:,0], prices.shape[0])
+    action 			three action
+                    0:	empty, don't open/close.
+                    1:	open a position
+                    2: 	keep a position
+    """
 
-			self.prices = prices.copy()
-			self.price = price/price[0]*100
-			self.t_max = len(self.price) - 1
+    def reset(self):
+        self.empty = True
+        self.max_reward = self.max_sample_RUL - self.window_state
+        self.t = self.t0
+        self.t_max = len(self.sample_data) - 1
+        return self.get_state(), self.valid_actions
 
-		self.max_profit = find_ideal(self.price[self.t0:], False)
-		self.t = self.t0
-		return self.get_state(), self.get_valid_actions()
+    def get_state(self, t=None):
+        if t is None:
+            t = self.t
+        state = self.sample_data[self.sensor_name_choice][t - self.window_state + 1: t + 1].copy()
+        norm = state.mean()
+        state = (state/norm-1)*100  # not sure why this is being done, maybe normalisation?
+        state = state.to_numpy()
 
+        # for i in range(1):
+        #     norm = np.mean(state[:, i])
+        #     state[:, i] = (state[:, i] / norm - 1.) * 100
+        return state
 
-	def get_state(self, t=None):
-		if t is None:
-			t = self.t
-		state = self.prices[t - self.window_state + 1: t + 1, :].copy()
-		for i in range(self.sampler.n_var):
-			norm = np.mean(state[:,i])
-			state[:,i] = (state[:,i]/norm - 1.)*100	
-		return state
+    def get_maintain_reward(self):
+        t = self.t
+        rul = self.max_sample_RUL - t
+        if rul > self.optimum_buffer:
+            reward = self.reward_increment
+        elif self.optimum_buffer >= rul >= self.window_state:
+            reward = self.reward_reduction
+        else:
+            reward = self.failure_cost
+        return reward
 
-	def get_valid_actions(self):
-		if self.empty:
-			return [0, 1]	# wait, open
-		else:
-			return [0, 2]	# close, keep
+    def get_replace_reward(self):
+        t = self.t
+        midway_point = int(self.max_sample_RUL/2)
+        if midway_point > t:
+            reward = self.failure_cost
+        else:
+            reward = self.reward_replace
+        return reward
 
+    def step(self, action):
+        done = False
+        if action == 0:  # maintain
+            reward = self.get_maintain_reward()
+        elif action == 1:  # replace
+            reward = self.get_replace_reward()
+        else:
+            raise ValueError('no such action: ' + str(action))
 
-	def get_noncash_reward(self, t=None, empty=None):
-		if t is None:
-			t = self.t
-		if empty is None:
-			empty = self.empty
-		reward = self.direction * (self.price[t+1] - self.price[t])
-		if empty:
-			reward -= self.open_cost
-		if reward < 0:
-			reward *= (1. + self.risk_averse)
-		return reward
+        self.t += 1
 
+        if self.t == self.t_max or action == 1:
+            done = True
+        return self.get_state(), reward, done, self.valid_actions
 
-	def step(self, action):
+    def sample(self):
+        unit_nr_list = list(self.df["unit_nr"].unique())
+        unit_nr_choice = random.choice(unit_nr_list)
+        sensor_name_choice = random.choice(self.sensor_names)
 
-		done = False
-		if action == 0:		# wait/close
-			reward = 0.
-			self.empty = True
-		elif action == 1:	# open
-			reward = self.get_noncash_reward()
-			self.empty = False
-		elif action == 2:	# keep
-			reward = self.get_noncash_reward()
-		else:
-			raise ValueError('no such action: '+str(action))
+        return(unit_nr_choice , sensor_name_choice)
 
-		self.t += 1
-		return self.get_state(), reward, self.t == self.t_max, self.get_valid_actions()
+    def __init__(self,
+                 csv_name,
+                 window_state,
+                 reward_increment,
+                 reward_replace,
+                 reward_reduction,
+                 failure_cost,
+                 optimum_buffer,
+                 n_action=2,
+                 n_var=1):
 
+        self.df = pd.read_csv(csv_name)
+        self.window_state = window_state
+        self.reward_increment = reward_increment
+        self.reward_replace = reward_replace
+        self.reward_reduction = reward_reduction
+        self.failure_cost = failure_cost
+        self.optimum_buffer = optimum_buffer
 
-	def __init__(self, 
-		sampler, window_state, open_cost,
-		direction=1., risk_averse=0.):
+        self.n_action = n_action
+        self.n_var = n_var
+        self.state_shape = (window_state, self.n_var)
+        self.action_labels = ['maintain', 'replace']
+        self.valid_actions = [0, 1]  # 0 for maintain, 1 for replace
+        self.t0 = window_state - 1
+        self.sensor_names = ['s_{}'.format(i) for i in range(1,22)]
 
-		self.sampler = sampler
-		self.window_state = window_state
-		self.open_cost = open_cost
-		self.direction = direction
-		self.risk_averse = risk_averse
-
-		self.n_action = 3
-		self.state_shape = (window_state, self.sampler.n_var)
-		self.action_labels = ['empty','open','keep']
-		self.t0 = window_state - 1
+        # self.unit_nr_choice, self.sensor_name_choice = self.sample()
+        self.unit_nr_choice = 1
+        self.sensor_name_choice = "s_4"
+        self.sample_data = self.df[self.df["unit_nr"] == self.unit_nr_choice][["unit_nr", self.sensor_name_choice, "RUL"]]
+        self.max_sample_RUL = self.sample_data["RUL"].max()
 
 
 if __name__ == '__main__':
-	test_env()
+    print(os.getcwd())
+
+    env = Market(csv_name="main_data.csv",
+                 window_state=10,
+                 reward_increment=10.,
+                 reward_replace=100,
+                 reward_reduction=-1000.,
+                 failure_cost=-200000.,
+                 optimum_buffer=30)
+
+    env.reset()
+    # print("state: {}".format(state),"\n","reward: {}".format(reward),"\n","t_max: {}".format(t_max))
+    # print(len(env.sample_data))
+    rewards = []
+    for i in range(env.max_sample_RUL):
+        action = random.choice([0, 1])
+        state, reward, _, _ = env.step(action=action)
+        print("reward: {}".format(reward),"\n")
+        rewards.append(reward)
+    print("total reward: {}".format(sum(rewards)))
+
+
